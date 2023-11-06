f0ae85523518d3547f59dff206526794d64c9831
alexdjdjx <117446274+alexdjdjx@users.noreply.github.com>
2023-11-06 15:53:39 +0100

    hola

--- Assets/Scripts/Movimiento_Personaje.cs
+++ Assets/Scripts/Movimiento_Personaje.cs
@@ -6,69 +6,52 @@ using UnityEngine;
 
 public class SC_FPSController : MonoBehaviour
 {
-    public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Camera playerCamera;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
+    public float velocidadCaminar = 7.5f;
+    public float velocidadCorrer = 11.5f;
+    public Camera camaraJugador;
+    public float sensibilidadRaton = 2.0f;
 
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
+    CharacterController controladorPersonaje;
+    Vector3 direccionMovimiento = Vector3.zero;
+    float rotacionX = 0;
 
     [HideInInspector]
-    public bool canMove = true;
+    public bool puedeMoverse = true;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        controladorPersonaje = GetComponent<CharacterController>();
 
-        // Lock cursor
+        // Bloquear cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        Vector3 adelante = transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical");
+        Vector3 derecha = transform.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal");
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        {
-            moveDirection.y = jumpSpeed;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
-        }
+        // Presiona Shift para correr
+        bool estaCorriendo = Input.GetKey(KeyCode.LeftShift);
+        float velocidadActualX = puedeMoverse ? (estaCorriendo ? velocidadCorrer : velocidadCaminar) : 0;
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
+        direccionMovimiento = (adelante + derecha).normalized * velocidadActualX;
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+        // Mover el controlador
+        controladorPersonaje.Move(direccionMovimiento * Time.deltaTime);
 
-        // Player and Camera rotation
-        if (canMove)
+        // Rotación del jugador y la cámara
+        if (puedeMoverse)
         {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            rotacionX += -Input.GetAxis("Mouse Y") * sensibilidadRaton;
+            rotacionX = Mathf.Clamp(rotacionX, -45f, 45f);
+            camaraJugador.transform.localRotation = Quaternion.Euler(rotacionX, 0, 0);
+
+          
         }
+          float rotacionY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensibilidadRaton;
+        transform.localEulerAngles = new Vector3(0, rotacionY, 0);
+        controladorPersonaje.Move(direccionMovimiento * Time.deltaTime);
     }
 }
